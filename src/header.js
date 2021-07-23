cdce1dc832fc249d6f75d8051bdf1e2de3fba2d7
jkh96 <jalin2583@yahoo.com>
2021-07-23 15:14:31 -0700

    Modules wip 01

--- src/header.js
+++ src/header.js
@@ -24,6 +24,72 @@ function header() {
     headerDiv.appendChild(i1)
     headerDiv.appendChild(i2)
     headerDiv.appendChild(i3)
+
+
+    // Create Aside Elements
+    const asideDiv = document.createElement('aside')
+    const ul = document.createElement('ul')
+    const liHome = document.createElement('li')
+    const liHomeI = document.createElement('i')
+    const liHomeP = document.createElement('p')
+    const liDT = document.createElement('li')
+    const liDTI = document.createElement('i')
+    const liDTP = document.createElement('p')
+    const liWT = document.createElement('li')
+    const liWTI = document.createElement('i')
+    const liWTP = document.createElement('p')
+    const liFav = document.createElement('li')
+    const liFavI = document.createElement('i')
+    const liFavP = document.createElement('p')
+    const liProjects = document.createElement('li')
+    const liProjectsI = document.createElement('i')
+    const liProjectsP = document.createElement('p')
+
+    // Create Aside Classes
+    liHome.classList.add('d-flex', 'flex-column')
+    liHomeI.classList.add('bi', 'bi-house')
+    liDT.classList.add('d-flex', 'flex-column')
+    liDTI.classList.add('bi', 'bi-calendar-event')
+    liWT.classList.add('d-flex', 'flex-column')
+    liWTI.classList.add('bi', 'bi-calendar2-week')
+    liFav.classList.add('d-flex', 'flex-column')
+    liFavI.classList.add('bi', 'bi-star')
+    liProjects.classList.add('d-flex', 'flex-column')
+    liProjectsI.classList.add('bi', 'bi-folder')
+
+    // Create Aside inline-styles
+    liHomeI.style.fontSize = '2rem'
+    liDTI.style.fontSize = '2rem'
+    liWTI.style.fontSize = '2rem'
+    liFavI.style.fontSize = '2rem'
+    liProjectsI.style.fontSize = '2rem'
+
+    // Create Aside Text
+    liHomeP.textContent = 'Home'
+    liDTP.innerText = 'Daily Task'
+    liWTP.innerText = 'Weekly Task'
+    liFavP.innerText = 'Favorites'
+    liProjectsP.innerText = 'Projects'
+
+    // Append Aside to container
+    htmlContainer.appendChild(asideDiv)
+    asideDiv.appendChild(ul)
+    ul.appendChild(liHome)
+    liHome.appendChild(liHomeI)
+    liHome.appendChild(liHomeP)
+    ul.appendChild(liDT)
+    liDT.appendChild(liDTI)
+    liDT.appendChild(liDTP)
+    ul.appendChild(liWT)
+    liWT.appendChild(liWTI)
+    liWT.appendChild(liWTP)
+    ul.appendChild(liFav)
+    liFav.appendChild(liFavI)
+    liFav.appendChild(liFavP)
+    ul.appendChild(liProjects)
+    liProjects.appendChild(liProjectsI)
+    liProjects.appendChild(liProjectsP)
+    
 }
 
 export {header}
--- src/home.js
+++ src/home.js
@@ -2,70 +2,6 @@ function home() {
         // Grab Content container
         let htmlContainer = document.getElementById('content')
 
-        // Create Aside Elements
-        const asideDiv = document.createElement('aside')
-        const ul = document.createElement('ul')
-        const liHome = document.createElement('li')
-        const liHomeI = document.createElement('i')
-        const liHomeP = document.createElement('p')
-        const liDT = document.createElement('li')
-        const liDTI = document.createElement('i')
-        const liDTP = document.createElement('p')
-        const liWT = document.createElement('li')
-        const liWTI = document.createElement('i')
-        const liWTP = document.createElement('p')
-        const liFav = document.createElement('li')
-        const liFavI = document.createElement('i')
-        const liFavP = document.createElement('p')
-        const liProjects = document.createElement('li')
-        const liProjectsI = document.createElement('i')
-        const liProjectsP = document.createElement('p')
-
-        // Create Aside Classes
-        liHome.classList.add('d-flex', 'flex-column')
-        liHomeI.classList.add('bi', 'bi-house')
-        liDT.classList.add('d-flex', 'flex-column')
-        liDTI.classList.add('bi', 'bi-calendar-event')
-        liWT.classList.add('d-flex', 'flex-column')
-        liWTI.classList.add('bi', 'bi-calendar2-week')
-        liFav.classList.add('d-flex', 'flex-column')
-        liFavI.classList.add('bi', 'bi-star')
-        liProjects.classList.add('d-flex', 'flex-column')
-        liProjectsI.classList.add('bi', 'bi-folder')
-
-        // Create Aside inline-styles
-        liHomeI.style.fontSize = '2rem'
-        liDTI.style.fontSize = '2rem'
-        liWTI.style.fontSize = '2rem'
-        liFavI.style.fontSize = '2rem'
-        liProjectsI.style.fontSize = '2rem'
-
-        // Create Aside Text
-        liHomeP.textContent = 'Home'
-        liDTP.innerText = 'Daily Task'
-        liWTP.innerText = 'Weekly Task'
-        liFavP.innerText = 'Favorites'
-        liProjectsP.innerText = 'Projects'
-
-        // Append Aside to container
-        htmlContainer.appendChild(asideDiv)
-        asideDiv.appendChild(ul)
-        ul.appendChild(liHome)
-        liHome.appendChild(liHomeI)
-        liHome.appendChild(liHomeP)
-        ul.appendChild(liDT)
-        liDT.appendChild(liDTI)
-        liDT.appendChild(liDTP)
-        ul.appendChild(liWT)
-        liWT.appendChild(liWTI)
-        liWT.appendChild(liWTP)
-        ul.appendChild(liFav)
-        liFav.appendChild(liFavI)
-        liFav.appendChild(liFavP)
-        ul.appendChild(liProjects)
-        liProjects.appendChild(liProjectsI)
-        liProjects.appendChild(liProjectsP)
-
 
         // Create Entire Main Section
 
@@ -85,10 +21,11 @@ function home() {
         const welcomeRowDiv = document.createElement('div')
         const welcomeColDiv = document.createElement('div')
         const welcomeH1 = document.createElement('h1')
+        const welcomeHR = document.createElement('hr')
     
         // Create Welcome User Classes
         welcomeRowDiv.classList.add('row', 'mb-4')
-        welcomeColDiv.classList.add('col', 'col-12')
+        welcomeColDiv.classList.add('col', 'col-12', 'text-light')
 
         // Create Welcome User Text
         welcomeH1.innerText = 'Welcome User!'
@@ -97,6 +34,7 @@ function home() {
         mainContainerFluid.appendChild(welcomeRowDiv)
         welcomeRowDiv.appendChild(welcomeColDiv)
         welcomeColDiv.appendChild(welcomeH1)
+        welcomeColDiv.appendChild(welcomeHR)
 
 
         // Create Cards
@@ -111,14 +49,56 @@ function home() {
         mainContainerFluid.appendChild(cardRowDiv)
 
 
-        // Create Card1Col
+        // Create Card Section 5
+        const card5Div = document.createElement('div')
+        const card5Body = document.createElement('div')
+        const card5Title = document.createElement('div')
+        const card5P = document.createElement('p')
+        const card5HR = document.createElement('hr')
+        const card5TextDiv = document.createElement('div')
+        const card5TextP = document.createElement('p')
+        const card5TextLI1 = document.createElement('li')
+        const card5TextLI2 = document.createElement('li')
+        const card5TextLI3 = document.createElement('li')
+        const card5TextLI4 = document.createElement('li')
+
+        // Create Card Section 5 Classes
+        card5Div.classList.add('card', 'col-5', 'me-3', 'mb-3', 'rct-info')
+        card5Body.classList.add('card-body')
+        card5Title.classList.add('card-title', 'text-center')
+        card5TextDiv.classList.add('card-text')
+
+        //  Add Card Card Section 5 Text
+        card5P.innerText = 'DashBoard'
+        card5TextP.innerText = 'Welcome To your DashBoard! On This Page you can see all of your importanted items such as:'
+        card5TextLI1.innerText = 'Recently completed Task'
+        card5TextLI2.innerText = 'Daily Task'
+        card5TextLI3.innerText = 'Weekly Task'
+        card5TextLI4.innerText = 'Projects'
+
+        // Append Card Section 5 to Card Col
+        cardRowDiv.appendChild(card5Div)
+        card5Div.appendChild(card5Body)
+        card5Body.append(card5Title)
+        card5Title.append(card5P)
+        card5Title.append(card5HR)
+        card5Body.appendChild(card5TextDiv)
+        card5TextDiv.appendChild(card5TextP)
+        card5TextDiv.appendChild(card5TextLI1)
+        card5TextDiv.appendChild(card5TextLI2)
+        card5TextDiv.appendChild(card5TextLI3)
+        card5TextDiv.appendChild(card5TextLI4)
+
+
+        // Create Card Col
         const cardColDiv = document.createElement('div')
 
-        // Create Card 1 Col Classes
+        // Create Card Col Classes
         cardColDiv.classList.add('col-5', 'mb-4', 'me-3', 'd-flex', 'justify-content-between', 'p-0')
 
-        // Append card1col to Card Row
+        // Append card Col to Card Row
         cardRowDiv.appendChild(cardColDiv)
+
 
         // Create Card Section 1
         const card1Div = document.createElement('div')
@@ -173,7 +153,7 @@ function home() {
         card2Title.classList.add('card-title', 'text-center')
 
         //  Add Card Card Section 2 Text
-        card2P.innerText = 'Upcompleted Task'
+        card2P.innerText = 'Daily Task'
 
         // Append Card Section 2 to Card Col
         cardColDiv.appendChild(card2Div)
@@ -196,7 +176,7 @@ function home() {
         card3Title.classList.add('card-title', 'text-center')
 
         //  Add Card Card Section 3 Text
-        card3P.innerText = 'Upcoming Task'
+        card3P.innerText = 'Weekly Task'
 
         // Append Card Section 3 to Card Col
         cardColDiv.appendChild(card3Div)
@@ -227,35 +207,6 @@ function home() {
         card4Body.append(card4Title)
         card4Title.append(card4P)
         card4Title.append(card4HR)
-
-
-        // Create Card Section 5
-        const card5Div = document.createElement('div')
-        const card5Body = document.createElement('div')
-        const card5Title = document.createElement('div')
-        const card5P = document.createElement('p')
-        const card5HR = document.createElement('hr')
-        const card5TextDiv = document.createElement('div')
-        const card5TextP = document.createElement('p')
-
-        // Create Card Section 5 Classes
-        card5Div.classList.add('card', 'col-5', 'rct', 'text-light')
-        card5Body.classList.add('card-body')
-        card5Title.classList.add('card-title', 'text-center')
-        card5TextDiv.classList.add('card-text')
-
-        //  Add Card Card Section 5 Text
-        card5P.innerText = 'Other'
-        card5TextP.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi repudiandae autem fugit nam, quisquam ratione nesciunt debitis omnis dignissimos sequi modi deleniti! Molestiae officia possimus, cupiditateerror dignissimos provident tenetur nulla nobis, assumenda rerum rem facilis minima cum aliquam quos sequi repellendus optio suscipit. Ea consequatur praesentium eaque quibusdam rem alias dolores, ipsa, officia possimus harum dolorum nostrum eos numquam voluptates sint eius itaque impedit non enim nisi aspernatur quo consequuntur autem quasi.'
-
-        // Append Card Section 5 to Card Col
-        cardRowDiv.appendChild(card5Div)
-        card5Div.appendChild(card5Body)
-        card5Body.append(card5Title)
-        card5Title.append(card5P)
-        card5Title.append(card5HR)
-        card5Body.appendChild(card5TextDiv)
-        card5TextDiv.appendChild(card5TextP)
 }
 
 export {home}
